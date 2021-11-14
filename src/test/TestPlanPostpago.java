4a0e08b1be915e5157239e57b8be368456adffb5
Gi0vs <53981859+Gi0vs@users.noreply.github.com>
2021-11-14 16:35:34 -0400

    pruebas de la clase plan Postpago creadas

---
+++ src/test/TestPlanPostpago.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import entidades.CDR;
+import planes.IPlan;
+import planes.PlanPostpago;
+import tarifaciones.TarifacionFijaPorMinuto;
+
+class TestPlanPostpago {
+	
+
+	@Test
+	@DisplayName("Calcular costo de llamada")
+	public void VerificarCalculoDeCosteDeCdr() {
+		CDR cdr = new CDR(70743567,70765342,"10/10/19","07:14","00:51:00",2.2);
+		IPlan planPostpago = new PlanPostpago();
+		assertEquals(73.95, planPostpago.calcularCostoDeLlamada(new TarifacionFijaPorMinuto(), cdr));
+	}
+	
+	@Test
+	@DisplayName("Obtener el Nombre del plan")
+	public void VerificarQueSeRetornaElNombeDelTipoDePlan(){
+		IPlan planPostpago = new PlanPostpago();
+		assertEquals("POSTPAGO", planPostpago.getTipoDePlan());
+	}
+
+}
